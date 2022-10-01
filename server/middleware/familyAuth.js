f37d597bb170cf6859b5218fa8045f4f43327d7c
TaherAfini <afinitaher@gmail.com>
2022-10-01 14:58:32 -0400

    Family login, logout, logoutAll, familyProfile

--- server/middleware/familyAuth.js
+++ server/middleware/familyAuth.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const jwt = require('jsonwebtoken');
 const Family = require('../models/family');
 const familyRef = db.collection('family');
@@ -7,15 +8,23 @@ const familyAuth = async (req, res, next) => {
     try{
         const token = req.header('Authorization').replace('Bearer ', '');
         const decoded = jwt.verify(token, process.env.CRYPT);
-        const family = await familyRef.where('id', '==', decoded.id).where('tokens', 'array-contains', token).get()
-        if(!family)
+        const family = await familyRef.where('id', '==', decoded.id).get()
+        if(family.empty)
             throw new Error()
+
         req.token = token;
-        req.member = member;
+        req.family = "";
+        family.forEach(doc => {
+            req.family = doc.data()
+        });
+
+        if(!req.family.tokens.includes(token)) {
+            throw new Error()
+        }
         next();
     }catch(error){
         res.status(401).send({error: 'Please Authenticate'});
     }
 }
 
-module.exports = auth;
+module.exports = familyAuth;
